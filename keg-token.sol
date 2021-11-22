345e6143b03d3a7b45651df2d52c5d5049f97a3a
Pete <petebarkley@gmail.com>
2021-11-22 17:27:59 -0500

    added brewer change

--- keg-token.sol
+++ keg-token.sol
@@ -3,20 +3,27 @@ pragma solidity ^0.8.7;
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
 contract KegToken is ERC20 {
-    address public admin;
-    
-    constructor() ERC20('Keg Tokens', 'KEGS'){
+
+    address public brewer;
+    string rejectMessage = 'No no no, only the brewer';
+
+    constructor() ERC20('Keg Tokens', 'KEG'){
         _mint(msg.sender, 100000 * 10 ** 2);
         admin = msg.sender;
     }
     
     function decimals() public view override returns (uint8) {
-		return 2;
+        return 2;
 	}
     
     function mint(address to, uint amount) external {
-        require(msg.sender == admin, 'No no no, only the brewer');
+        require(msg.sender == brewer, rejectMessage);
         _mint(to, amount);
+    }
+
+    function changeBrewer(address newBrewer) external {
+        require(msg.sender == brewer, rejectMessage);
+        this.brewer = newBrewer;
     }
     
     function burn(uint amount) external {
