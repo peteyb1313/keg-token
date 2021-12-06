68b221a9aa0dc18cb76effdfe4e24e669698d718
Pete <petebarkley@gmail.com>
2021-12-05 20:48:02 -0500

    explain tokens

--- keg-token.sol
+++ keg-token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.7;
 // Base BeerToken
 import './beer-token.sol';
 
+//Let's create a BeerToken for tracking KEGs with an initial supply of 10 KEGs which can be divided by 2 decimal places
 contract KegToken is BeerToken('Keg Tokens', 'KEG', 10, 2) {
     
     constructor() {}
