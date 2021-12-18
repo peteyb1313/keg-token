d1a08907b98a6421a114273e4162f72590d5ce9e
Pete <petebarkley@gmail.com>
2021-12-18 18:13:33 -0500

    Added web3 beer listener and google home devide talker

---
+++ beer-listener.js
@@ -0,0 +1,415 @@
+
+var Web3 = require('web3');
+var Contract = require('web3-eth-contract');
+
+
+const web3 = new Web3("wss://ropsten.infura.io/ws/v3/9d558226a6364ce6a821646ee8c51806");
+
+// Pete Main: 0x03509C9F6efB20F70181405DEd5FE1a0f52216D9
+// Doc: 0xEDb31a4e1BA43701402D60DcB089A0BAE0181ddF
+
+console.log("Starting event query...");
+eventQuery();
+
+
+async function eventQuery(){
+
+    var contract = new Contract([
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_tokenName",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_tokenCode",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_tokenCount",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint8",
+                    "name": "_decimalPlaces",
+                    "type": "uint8"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Approval",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Transfer",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                }
+            ],
+            "name": "allowance",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "account",
+                    "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "brewBeer",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "brewer",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "newBrewer",
+                    "type": "address"
+                }
+            ],
+            "name": "changeBrewer",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "decimals",
+            "outputs": [
+                {
+                    "internalType": "uint8",
+                    "name": "",
+                    "type": "uint8"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "subtractedValue",
+                    "type": "uint256"
+                }
+            ],
+            "name": "decreaseAllowance",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "addedValue",
+                    "type": "uint256"
+                }
+            ],
+            "name": "increaseAllowance",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "name",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "account",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "revokeBeer",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "symbol",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "totalSupply",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transfer",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "sender",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "amount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transferFrom",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ], '0x1b12242029405ceb7e1c672e9a6e3c784c1b1865');
+
+    contract.setProvider(web3);
+
+    console.log("Contract Address: " +contract.options.address);
+
+    console.log("Getting brewer...");
+
+    var brewer = await contract.methods.brewer().call(function(error, result) {
+        if(error != null)
+            console.log("Error getting brewer: " + error)
+        return result;
+    });
+
+    if(brewer == null) {
+        console.log("Could not get brewer, exiting.");
+        process.exit(1);
+    }
+
+    console.log("Brewer: " + brewer);
+
+    console.log("Waiting for beer transfer events...");
+
+    contract.events.Transfer()
+        .on('data', (event) => {
+            var sender = event.returnValues.from;
+            var receiver = event.returnValues.to;
+            var numberOfPints = event.returnValues.value;
+            
+            console.log("Pint Tokens Sent!");
+            console.log("Sender: " + sender);
+            console.log("Receiver:" +receiver);
+            console.log("Number of Pints:" +numberOfPints);
+
+            if(receiver == brewer) {
+                console.log("This is a beer redemption from the brewer. Enable the taps!");
+            }
+        })
+        .on('error', console.error);
+
+}
+
+
+
+
---
+++ google-interactions.js
@@ -0,0 +1,60 @@
+var Client = require('castv2-client').Client;
+var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+const googleTTS = require('google-tts-api');
+
+var App = {
+    playin: false,
+    DeviceIp: "",
+    Player: null,
+    GoogleHome: function (host, url) {
+        console.log("Starting home call: " +this.DeviceIp);
+        var client = new Client();
+        client.connect(host, function () {
+            console.log("Connected");
+            client.launch(DefaultMediaReceiver, function (err, player) {
+                console.log(err);
+                console.log("Launhcned setting vaolume");
+
+                var media = {
+                    contentId: url,
+                    contentType: 'audio/mp3',
+                    streamType: 'BUFFERED'
+                };
+                App.Player = player;
+                App.Player.load(media, { autoplay: true }, function (err, status) {
+                    App.Player.on('status', function (status) {
+                        if (status.playerState === "IDLE" && App.playin === false) {
+                            client.close();
+                        }
+                    });
+                });
+            });
+        });
+        client.on('error', function (err) {
+            console.log('Error: %s', err.message);
+            client.close();
+        });
+    },
+    run: function (ip, text) {
+        console.log("Initiating text to speech")
+        App.DeviceIp = ip;
+        const url = googleTTS.getAudioUrl(text, {
+            lang: 'en-US',
+            slow: false,
+            host: 'https://translate.google.com',
+        });
+        console.log("Calling home");
+        App.GoogleHome(App.DeviceIp, url, function (res) {
+            console.log(res);
+        })
+    },
+    broadcast: function(text){
+        const ips = process.env.GOOGLEHOME_IPS.split(","); //From config, 192.168.68.105,192.168.68.107,192.168.68.124
+        for (var s of ips){
+            App.run(s, text);
+        }
+    }
+};
+console.log("Running app");
+App.run("192.168.0.156", "Hello Sam");
+//App.broadcast("Broadcasted to all of the devices"); //Only works if you did step 4.5
